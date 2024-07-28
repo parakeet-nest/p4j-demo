47d8692025dcc3e8c1d6d1bfcb34a917c972ac01
@k33g <ph.charriere@gmail.com>
2024-07-28 08:54:44 +0200

    🛟 Updated.

---
+++ src/main/java/chat/DemoChat.java
@@ -0,0 +1,39 @@
+package chat;
+
+import org.parakeetnest.parakeet4j.llm.Message;
+import org.parakeetnest.parakeet4j.llm.Options;
+import org.parakeetnest.parakeet4j.llm.Query;
+
+import java.util.List;
+
+import static org.parakeetnest.parakeet4j.completion.Completion.Chat;
+
+public class DemoChat
+{
+    public static void main( String[] args )
+    {
+
+        Options options = new Options()
+                .setTemperature(0.0)
+                .setRepeatLastN(2);
+
+        var systemContent = "You are a useful AI agent, expert with the Star Trek franchise.";
+        var userContent = "Who is James Tiberius Kirk?";
+
+        List<Message> messages = List.of(
+                new Message("system", systemContent),
+                new Message("user", userContent)
+        );
+
+        Query queryChat = new Query("tinyllama", options)
+                .setMessages(messages);
+
+        Chat("http://0.0.0.0:11434", queryChat,
+                answer -> {
+                    System.out.println("🙂: " + answer.getMessage().getContent());
+                },
+                err -> {
+                    System.out.println("😡: " + err.getMessage());
+                });
+    }
+}
---
+++ src/main/java/chat/DemoChatStreamWithMemory.java
@@ -0,0 +1,75 @@
+package chat;
+
+import org.parakeetnest.parakeet4j.llm.Message;
+import org.parakeetnest.parakeet4j.llm.Options;
+import org.parakeetnest.parakeet4j.llm.Query;
+
+import java.util.List;
+
+import static org.parakeetnest.parakeet4j.completion.Completion.ChatStream;
+
+public class DemoChatStreamWithMemory
+{
+    public static void main( String[] args )
+    {
+        Options options = new Options()
+                .setTemperature(0.0)
+                .setRepeatLastN(2);
+
+        var systemContent = "You are a useful AI agent, expert with the Star Trek franchise.";
+        var userContent = "Who is James Tiberius Kirk?";
+
+        List<Message> messages = new java.util.ArrayList<>(List.of(
+                new Message("system", systemContent),
+                new Message("user", userContent)
+        ));
+
+        Query queryChat = new Query("tinyllama", options, messages);
+
+        var resultAnswer =
+                ChatStream("http://0.0.0.0:11434", queryChat,
+                chunk -> {
+                    System.out.print(chunk.getMessage().getContent());
+                    return null;
+                });
+
+        if(resultAnswer.exception().isPresent()) {
+            System.out.println(
+                    "😡: " + resultAnswer.exception().toString()
+            );
+            System.exit(1);
+        }
+
+        // Add the answer to the list of the messages
+        messages.add(
+                new Message("ai",
+                        resultAnswer.getAnswer().getMessage()
+                                .getContent()
+                )
+        );
+
+        var nextUserContent = "Who is his best friend?";
+
+        // Add the new question to the list of the messages
+        messages.add(new Message("user", nextUserContent));
+
+        Query nextQueryChat = new Query("tinyllama", options, messages);
+
+        System.out.println();
+        System.out.println("--------------------------------------");
+
+        var nextResultAnswer =
+                ChatStream("http://0.0.0.0:11434", nextQueryChat,
+                chunk -> {
+                    System.out.print(chunk.getMessage().getContent());
+                    return null;
+                });
+
+        if(nextResultAnswer.exception().isPresent()) {
+            System.out.println(
+                    "😡: " + nextResultAnswer.exception().toString()
+            );
+            System.exit(1);
+        }
+    }
+}
